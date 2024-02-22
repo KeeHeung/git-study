71c0a899d49ea06203644f910329155eec26c3bb
keeheung <xl7923@naver.com>
2024-02-23 01:29:17 +0900

    new file, file modified

--- src/main/java/com/example/gitstudy/GitstudyApplication.java
+++ src/main/java/com/example/gitstudy/GitstudyApplication.java
@@ -8,6 +8,7 @@ public class GitstudyApplication {
 
     public static void main(String[] args) {
         System.out.println("new line");
+        System.out.println("modified");
         SpringApplication.run(GitstudyApplication.class, args);
     }
 
